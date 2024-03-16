a3c60b8b828db10a5f1d3128301bd58ea6180da7
Prakash chaudhary <jprakash857@gmail.com>
2024-03-16 09:14:12 +0545

    Created register functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register',[UserController::class, 'loadregister']);
+Route::post('/register',[UserController::class, 'storeregister']);
+Route::get('/remember-token/{token}',[UserController::class, 'verifynow']);
